6f4392e9d715091c9d7a88d60303d33bca43d0e9
Onore849 <rion3376@gmail.com>
2020-09-25 18:02:33 +0900

    Initial Commit

---
+++ CustomBarCharts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomBarCharts
+//
+//  Created by 野澤拓己 on 2020/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomBarCharts/CustomBarChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomBarChartsApp.swift
+//  CustomBarCharts
+//
+//  Created by 野澤拓己 on 2020/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomBarChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
